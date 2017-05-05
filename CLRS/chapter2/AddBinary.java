9f04f378fbbfd3b1214707b19df00ab609cc63ce
lsry <984597130@qq.com>
2017-05-06 00:16:18 +0800

    add CLRS/chapter2/AddBinary.java CLRS/chapter2/question.md modify README.md

---
+++ CLRS/chapter2/AddBinary.java
@@ -0,0 +1,29 @@
+public class  AddBinary
+{
+	public static void addBin(int[] a,int[] b){
+		int i,n,key;
+		n=a.length;
+		key=0;
+		int[] c = new int[n+1];
+		for(int cv:c)
+			cv=0;
+		for (i=n-1;i>=0 ; i--)
+		{
+			c[i+1]=(a[i]+b[i]+key)%2;
+			//if((a[i]&key|b[i]&key|a[i]&b[i])==1)
+			//	key=1;   //р╡©ирт
+			key=(a[i]+b[i]+key)/2;
+			System.out.println(c[i+1]);
+		}
+		c[0]=key;
+		for(int cv:c)
+			System.out.print(cv);
+		System.out.println();
+	}
+	public static void main(String[] args) 
+	{
+		int[] a={0,1,1,0};
+		int[] b={1,1,1,0};
+		addBin(a,b);
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Practice
-几行代码随手写的
+> + 平时学习到的一些算法，代码技巧，注意事项等
++ 学习CLRS的内容
++ 练习LeetCode的内容  
+
+## CLRS
+> 1. 第二章
+
+## LeetCode
+> 1. Two Sum
+2. Integer Reverse
