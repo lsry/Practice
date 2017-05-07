34926be4c211040308f7798e0f7c85faadc0bfa9
lsry <984597130@qq.com>
2017-05-07 23:08:43 +0800

    add 2.2-2

---
+++ CLRS/chapter2/ChooseSort.java
@@ -0,0 +1,30 @@
+public class ChooseSort
+{
+	public static void ChooseSort(int[] a){
+         int n=a.length;
+		 for (int i=0;i<n-1 ;i++ )
+		 {
+			 int key = a[i];
+			 for (int j=i+1;j<n ;j++ )
+			 {
+				 if (key > a[j])
+				 {
+          int temp = a[j];
+					a[j] = key;
+					key = temp;
+				 }
+			 }
+			 a[i] = key;
+		 }
+	}
+
+	public static void main(String[] args)
+	{
+		int[] a = {0,4,2,6,8,2,7,5};
+		ChooseSort(a);
+		for (int ai : a)
+		{
+			System.out.print(ai + " ");
+		}
+	}
+}
