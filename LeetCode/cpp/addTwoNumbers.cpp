0e138c12f779fc47fa6e8f8e9a34af6a910d54ad
lsry <984597130@qq.com>
2018-01-06 21:03:15 +0800

    add OrdMartixMul.java

--- LeetCode/cpp/addTwoNumbers.cpp
+++ LeetCode/cpp/addTwoNumbers.cpp
@@ -7,9 +7,53 @@
      ListNode(int x) : val(x), next(NULL) {}
  };
 
- ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-     
- }
+ int getLength(ListNode* l){
+        int i = 0;
+        ListNode *p = l;
+        while(p!=NULL)
+        {
+            i++;
+            p = p->next;
+        }
+        return i;
+    }
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode *p = NULL,*q = NULL;
+        int len1 = getLength(l1);
+        int len2 = getLength(l2);
+        len1 >= len2 ?(p = l1,q = l2):(p = l2,q = l1);
+        int m = 0;
+        while (p != NULL && q != NULL){
+            int temp = (p->val+q->val + m)%10;
+            m = (p->val + q->val + m)/10;
+            p->val = temp;
+            if (p->next == NULL && m > 0)
+	        {
+		       ListNode *node = new ListNode(m);
+		       p -> next = node;
+	        }
+            p = p->next;
+            q = q->next;
+        }
+        while(m!=0&&len1!=len2){
+            int k = (p->val+ m)%10;
+            m = (p->val+ m)/10;
+            p-> val = k;
+            if (p->next == NULL && m > 0)
+	        {
+		       ListNode *node = new ListNode(m);
+		       p -> next = node;
+               m = 0;
+	        }
+            p = p->next;
+        }
+        if(len1 >= len2){
+            return l1;
+        }
+            
+        else
+            return l2;
+    }
 
 int main()
 {
