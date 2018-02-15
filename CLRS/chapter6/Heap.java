b7d499661e81c780bcb222df9e57decb5e469703
lsry <984597130@qq.com>
2018-02-15 20:28:12 +0800

    堆排序

---
+++ CLRS/chapter6/Heap.java
@@ -0,0 +1,147 @@
+public class Heap
+{
+    private final static int ARRAY_LENGTH = 50;       //可容纳元素数
+	private int heap_size;                            //已经容纳元素数
+	private int[] heapArray;
+
+    /*
+	* @param arr[] 传入的数组
+	* @param n 数组长度
+	*/
+	public Heap(int arr[],int n){
+		heapArray = new int[ARRAY_LENGTH];
+		heap_size = n;
+		for (int i = 0;(i < n)&&(i < ARRAY_LENGTH) ;i++)
+		{
+			heapArray[i] = arr[i];
+		}
+	}
+
+   /*
+	* 获得父元素索引
+	* @param in 需要确定元素位置的索引
+	*/
+    public int parantIndex(int in){
+		double x = in/2.0+0.5;
+		int d = (int)x - 1;
+	    return d;
+	}
+    
+	/*
+	* 获得左孩子元素索引
+	* @param in 需要确定元素位置的索引
+	*/
+	public int leftChildIndex(int in){
+		return in * 2 + 1;
+	}
+
+    /*
+	* 获得右孩子元素索引
+	* @param in 需要确定元素位置的索引
+	*/
+	public int rightChildIndex(int in){
+	    return in * 2 + 2;
+	}
+
+    /*
+	* 递归版本使第in元素维护最大堆的性质
+	* @param in 需要确定元素位置的索引
+	*/
+    public void maxHeapify(int in){
+	    int left = leftChildIndex(in);
+		int right = rightChildIndex(in);
+		int largest = in;
+		if ((left < heap_size)&&(heapArray[left] > heapArray[largest]))
+		{
+			largest = left;
+		}
+		if ((right < heap_size)&&(heapArray[right] > heapArray[largest]))
+		{
+			largest = right;
+		}
+		if (largest != in)
+		{
+			int temp = heapArray[in];
+			heapArray[in] = heapArray[largest];
+			heapArray[largest] = temp;
+			maxHeapify(largest);
+		}
+	}
+
+    /*
+	* 循环版本使第in元素维护最大堆的性质
+	* @param in 需要确定元素位置的索引
+	*/
+    public void maxHeapifyNotRec(int in){
+	    int left = leftChildIndex(in);
+		int right = rightChildIndex(in);
+		int largest = in;
+		while ((left < heap_size)&&(heapArray[left] > heapArray[largest])||(right < heap_size)&&(heapArray[right] > heapArray[largest]))
+		{
+			int inn = largest;
+			if ((left < heap_size)&&(heapArray[left] > heapArray[largest]))
+		    {
+			    largest = left;
+		    }
+			if ((right < heap_size)&&(heapArray[right] > heapArray[largest]))
+		    {
+			    largest = right;
+		    }
+			if (largest != inn)
+		    {
+			    int temp = heapArray[inn];
+			    heapArray[inn] = heapArray[largest];
+			    heapArray[largest] = temp;
+				left = leftChildIndex(largest);
+		        right = rightChildIndex(largest);
+		    } 
+		}
+	}
+    
+	/*
+	* 构建最大堆
+	*/
+    public void buildMaxHeap(){
+	    for (int i = (heap_size-1)/2;i >= 0;i--)
+	    {
+            maxHeapifyNotRec(i);
+	    }
+	}
+
+    /*
+	* 堆排序算法
+	*/
+	public void heapSort(){
+	    buildMaxHeap();
+		int heapSize = heap_size;
+		for (int i = heap_size-1;i>0;i--)
+		{
+			int temp = heapArray[0];
+			heapArray[0] = heapArray[i];
+			heapArray[i] = temp;
+			heap_size--;
+            maxHeapifyNotRec(0);
+		}
+		heap_size = heapSize;
+	}
+
+	public void display(){
+	    for (int i = 0;i < heap_size ;i++)
+	    {
+			System.out.print(heapArray[i] + "\t");
+	    }
+		System.out.println();
+	}
+
+
+
+
+	public static void main(String[] args) 
+	{
+		int arr[] = new int[]{4,2,5,7,9,2,1,8};
+		Heap he = new Heap(arr,8);
+		he.display();
+		he.heapSort();
+		he.display();
+	}
+}
--- CLRS/chapter6/README.md
+++ CLRS/chapter6/README.md
@@ -1,5 +1,92 @@
 ## Chapter 6  堆排序
 
 ### 6.1 堆
+根据书中对堆高度的定义，最底下一层为第0层，倒数第二层为第1层，。。。，所以根节点处为第h层。     
+某一个结点的高度为该结点到叶结点最长简单路径的边数，并不等于该层的高度。
 
 #### 6.1-1
+个数最多：表示最底下一层是满的，所以：Nmax = 1 + 2 + 2^2 + ... + 2^h = 2^(h+1)-1      
+个数最少：表示最底下一层只有一个元素，所以：Nmin = 1 + 2 + 2^2 + ... + 2^(h-1) + 1 = 2^h       
+
+#### 6.1-2    
+当最底下一层是满的，即 2^(h+1)-1 = n --> h = lg(n+1)-1    
+最底下一层只有一个元素: 2^h = n --> h = lgn    
+一般最底下一层元素数： 1 ≤ n0 ≤ 2^h ,所以高度取 floor(lgn)    
+
+#### 6.1-3   
+证明：假设最大元素不在该子树的根节点上，不妨设为A[i]，i ≠ 1，  则A[i]有父元素，且A[i]≥A[parant(i)]，与堆的性质不符合，所以该子树的最大元素必然在该子树的根节点上。   
+
+#### 6.1-4    
+叶结点处
+
+#### 6.1-5
+是    
+
+#### 6.1-6
+不是，6为7的父结点       
+
+#### 6.1-7      
+证明：首先在第n元素后就没有元素了，所以第n元素必在叶结点上，第n元素的父结点位置为floor(n/2),所以在floor(n/2)之后的元素都没有孩子结点，因此叶结点的下标为floor(n/2)+1，floor(n/2)+2, ... , n 。     
+
+### 6.2 维护堆的性质    
+
+#### 6.2-1
+![621](img/621.jpg)
+
+#### 6.2-2
+
+    MIN-HEAPIFY(A,i)
+    l = LEFT(i)
+    r = RIGHT(i)
+    if l ≤ A.heap-size and A[l] < A[i]    
+      smallest = l    
+    else
+      smallest = i
+    if r ≤ A.heap-size and A[r] < A[i]
+      smallest = r
+    if i != smallest
+      exchange A[i] and A[smallest]
+      MIN-HEAPIFY(A,smallest)
+
+T(n) = O(lgn)     
+
+#### 6.2-3
+在和左右孩子比较完后结束     
+
+#### 6.2-4
+得到没有左右孩子，而结束   
+
+#### 6.2-5
+
+    MAX_HEAPIFYNotRec(A,i)
+    l = LEFT(i)
+    r = RIGHT(i)
+    largest = i
+    //在存在左右孩子元素值大于根元素时开始循环
+    while r ≤ A.heap-size and A[r] > A[largest] or l ≤ A.heap-size and A[l] > A[largest]
+      i1 = largest
+      if l ≤ A.heap-size and A[l] > A[largest]
+        largest = l
+      if r ≤ A.heap-size and A[r] > A[largest]
+        largest = r
+      if largest != i1    
+        exchange A[largest] and A[i1]
+        l = LEFT(i)
+        r = RIGHT(i)
+
+#### 6.2-6
+证明：当某一个堆沿着最左边的时候，孩子元素都比根元素大的时候，A[lchild] > A[rchild] > A[parant] ,根元素值下降到最底层，此时为最坏的情况，运行时间为树的高度，Ω(lgn)      
+
+### 6.3 建堆   
+
+#### 6.3-1
+![631](img/631.jpg)
+
+#### 6.3-2
+递减可以保证i之后的元素作为根形成的子树是一个最大堆，当其递增时，在i之后的元素无法交换到i之前的位置，可能导致从底部升上来的最大元素无法达到应该在的位置。
+
+#### 6.3-3
+证明：当某一个高度h有最多的元素，那么该行是满的，有2^h个元素，而且从h , h+1 , ... , hmax 这些行都应该是满的，另外高度为h-1行应该最多有一个元素，      
+∴ 1 + 2 + 2^2 + ... + 2^h + 1 = n   
+
+### 6.4 堆排序算法
