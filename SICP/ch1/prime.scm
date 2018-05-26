df617870266a42ea9ff2e30e2f1637d031704556
lsry <lsrycx@qq.com>
2018-05-26 18:05:31 +0800

    1.22 1.23

--- SICP/ch1/prime.scm
+++ SICP/ch1/prime.scm
@@ -51,3 +51,74 @@
   )
 )
 
+; 1.22 exame time
+(define (search-for-primes n t)
+  (define (report-prime x elapsed-time)
+	  (newline)
+	  (display x)
+    (display " *** ")
+		(display elapsed-time)
+  )
+
+	(define (start-time-prime x start-time)
+	  (if (prime? x) (report-prime x (- (runtime) start-time))
+	  )
+	)
+
+	(define (isprime x y)
+	  (start-time-prime x (runtime))
+    (search-for-primes (+ 1 x) (- y 1))
+	)
+	
+	(cond ((and (> t 0) (prime? n)) (isprime n t))
+	  ((> t 0) (search-for-primes (+ n 1) t))
+	)
+)
+
+; 1.23 
+; next: using x = x + 2 (x != 2) 
+(define (is-prime n)
+  (define (next x)
+    (if (= x 2) (+ 1 x) (+ 2 x)
+    )
+  )
+
+	(define (divisor0 x test)
+	  (cond ((> (* test test) x) x)
+		  ((divides? x test) test)
+			(else (divisor0 x (next test)))
+	  )
+	)
+
+	(define (divides? a b)
+	    (= (remainder a b) 0)
+	)
+  
+  (define (smallest-divisor2 x)
+	  (divisor0 x 2)
+  )
+  (= n (smallest-divisor2 n))
+)
+
+(define (search-primes1 n t)
+  (define (report-prime x elapsed-time)
+	  (newline)
+	  (display x)
+    (display " *** ")
+		(display elapsed-time)
+  )
+
+	(define (start-time-prime x start-time)
+	  (if (is-prime x) (report-prime x (- (runtime) start-time))
+	  )
+	)
+
+	(define (isprime x y)
+	  (start-time-prime x (runtime))
+    (search-primes1 (+ 1 x) (- y 1))
+	)
+	
+	(cond ((and (> t 0) (is-prime n)) (isprime n t))
+	  ((> t 0) (search-primes1 (+ n 1) t))
+	)
+)
