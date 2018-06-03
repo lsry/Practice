2cf3ba158a9486991b6df43c2ca517c698328d2f
lsry <lsrycx@qq.com>
2018-06-03 22:32:36 +0800

    start 2.1

---
+++ SICP/ch2/code/rat.scm
@@ -0,0 +1,42 @@
+(define (make-rat n d)
+  (define (gcd a b)
+    (if (= 0 b) a (gcd b (remainder a b)))
+  )
+  (let ((g (gcd n d))) (cons (/ n g) (/ d g)))
+)
+
+(define (numer x)
+  (car x)
+)
+
+(define (denom x)
+  (cdr x)
+)
+
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y)) (* (numer y) (denom x))) (* (denom x) (denom y)))
+)
+
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y)) (* (numer y) (denom x))) (* (denom x) (denom y)))
+)
+
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y)) (* (denom x) (denom y)))
+)
+
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y)) (* (numer y) (denom x)))
+)
+
+(define (equal-rat? x y)
+  (= (* (numer x) (denom y)) (* (numer y) (denom x)))
+)
+
+(define (print-rat x)
+  (newline)
+	(display (numer x))
+	(display "/")
+	(display (denom x))
+)
+
