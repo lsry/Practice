327f9e658db62a30da2ab549793db6eec0c42204
lsry <lsrycx@qq.com>
2018-06-16 22:23:09 +0800

    2.32

--- SICP/ch2/code/mobile.scm
+++ SICP/ch2/code/mobile.scm
@@ -16,17 +16,66 @@
   (car (cdr mo))
 )
 
-(define (branch-length mo)
-  (car mo)
+(define (branch-length bn)
+  (car bn)
 )
 
-(define (branch-struc mo)
-  (if (pair? (cdr mo)) (car (cdr mo)) (cdr mo))
+(define (branch-struc bn)
+  (car (cdr bn))
 )
 
 ; b 
 (define (total-weight mo)
   (cond ((null? mo) 0)
+        ((not (pair? mo)) mo)
+        (else (+ (* (branch-length (left-branch mo)) (total-weight (branch-struc (left-branch mo))))
+                 (* (branch-length (right-branch mo)) (total-weight (branch-struc (right-branch mo))))
+              )
+        )
   )
 )
 
+; c 
+(define (balance? mo)
+  (define (branch-weight bn)
+    (* (branch-length bn) 
+       (if (pair? (branch-struc bn)) (total-weight (branch-struc bn)) (branch-struc bn))
+    )
+  )
+  (if (null? mo) #t
+      (and (= (branch-weight (left-branch mo)) (branch-weight (right-branch mo))) 
+           (if (pair? (branch-struc (left-branch mo))) (balance? (branch-struc (left-branch mo))) #t)
+           (if (pair? (branch-struc (right-branch mo))) (balance? (branch-struc (right-branch mo))) #t)      
+      )      
+  )
+)
+
+; d   
+(define (make-mobile2 left right)
+  (cons left rgiht)
+)
+
+(define (make-branch2 lengths struc)
+  (cons lengths struc)
+)
+
+; a 
+(define (left-branch2 mo)
+  (car mo)
+)
+
+(define (right-branch2 mo)
+  (cdr mo)
+)
+
+(define (branch-length2 bn)
+  (car bn)
+)
+
+(define (branch-struc2 bn)
+  (cdr bn)
+)
+
+(define x (make-mobile (make-branch 2 3) (make-branch 4 5))) 
+(define y (make-mobile (make-branch 2 6) (make-branch 4 3)))
+(define z (make-mobile (make-branch 4 (make-mobile (make-branch 2 4) (make-branch 1 8))) (make-branch 8 8)))
--- SICP/ch2/code/tree.scm
+++ SICP/ch2/code/tree.scm
@@ -27,5 +27,45 @@
   )
 )
 
+(define (scale-tree1 tree fac)
+  (cond ((null? tree) tree)
+        ((not (pair? tree)) (* tree fac))
+        (else (cons (scale-tree1 (car tree) fac) (scale-tree1 (cdr tree) fac)))
+  )
+)
 
-(define x (list (list 1 2) (list 3 4)))
+(define (scale-tree2 tree fac)
+  (map (lambda (sub-tree) (if (pair? sub-tree) (scale-tree2 sub-tree fac) (* fac sub-tree)))
+       tree
+  )
+)
+
+; 2.30 
+(define (square-tree tree)
+  (map (lambda (sub-tree) (if (pair? sub-tree) (square-tree sub-tree) (* sub-tree sub-tree)))
+       tree
+  )
+)
+
+; 2.31 
+(define (tree-map1 proc tree)
+  (map (lambda (sub-tree) (if (pair? sub-tree) (tree-map1 proc sub-tree) (proc sub-tree)))
+       tree
+  )
+)
+
+; 2.32  
+(define (subsets s)
+  (if (null? s) (list '()) 
+      (let ((rest (subsets (cdr s)))) 
+           (append rest 
+             (map (lambda (sub) (cons (car s) sub)) rest)
+           )
+      )
+  )
+)
+
+
+(define x (list 1 (list 2 (list 3 4) 5) (list 6 7)))
+
+(subsets (list 1 2 3))
