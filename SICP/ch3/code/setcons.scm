671ff6275d72e9291bdfa698548585483d89f7ef
lsry <lsrycx@qq.com>
2018-09-12 22:18:37 +0800

    complete 3.3.1

--- SICP/ch3/code/setcons.scm
+++ SICP/ch3/code/setcons.scm
@@ -49,3 +49,83 @@
 ; maxmum depth recursion  
 (define s4 (list 'a 'b 'c))
 (make-cycle s4)
+
+; 3.17  
+(define (count-pairs-true ls)
+  (define (add-cons x lis)
+    (if (and (pair? x) (not (memq x lis)))
+        (add-cons (car x) (add-cons (cdr x) (cons x lis)))
+        lis
+    )
+  )
+  (length (add-cons ls '()))
+)
+
+; 3.18
+(define (is-cycle ls)
+   (define me-ls '())
+   (define (add-ls lis)
+     (cond ((null? lis) #f)
+           ((not (pair? lis)) #f)
+           ((memq lis me-ls) #t)
+           (else (begin (set! me-ls (cons lis me-ls)) (or (add-ls (car lis)) (add-ls (cdr lis)) #f)))
+     )
+   )
+   (add-ls ls)
+)
+
+; 3.19  
+(define (is-cycle2 ls)
+  (define (walk num ls)
+    (cond ((null? ls) '())
+          ((= 0 num) ls)
+          (else (walk (- num 1) (cdr ls)))
+    )
+  )
+  
+  (define (run ls1 ls2)
+    (let ((p1 (walk 1 ls1))
+          (p2 (walk 2 ls2))
+         ) 
+         (cond ((or (null? p1) (null? p2)) #f)
+               ((eq? p1 p2) #t)
+               (else (run p1 p2))
+         ) 
+     )
+  )
+  (run ls ls)
+)
+
+(define (my-cons x y)
+  (define (set-x! v)
+    (set! x v)
+  )
+  (define (set-y! v)
+    (set! y v)
+  )
+  (define (dispatch m)
+    (cond ((eq? m 'cxr) x)
+          ((eq? m 'cyr) y)
+          ((eq? m 'set-x!) set-x!)
+          ((eq? m 'set-y!) set-y!)
+          (else (error "WRONG -- DISPATCH" m))
+    )
+  )
+  dispatch
+)
+
+(define (cxr z)
+  (z 'cxr) 
+)
+
+(define (cyr z)
+  (z 'cyr)
+)
+
+(define (set-x! z v)
+  ((z 'set-x!) v)
+)
+
+(define (set-y! z v)
+  ((z 'set-y!) v)
+)
