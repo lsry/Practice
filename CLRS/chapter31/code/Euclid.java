0ebee72346f540b54549176293edcd8f2e7000c3
lsry <lsrycx@qq.com>
2018-10-30 21:17:59 +0800

    31.4

--- CLRS/chapter31/code/Euclid.java
+++ CLRS/chapter31/code/Euclid.java
@@ -7,6 +7,11 @@ public class Euclid{
         }
     }
 
+    /*
+    * 递归方法解出d = gcd(a , b), 
+    * 满足ax' + by' = d 的所有整数解 x' , y'
+    * @return arr : d = arr[0] , x' = arr[1] , y' = arr[2]
+    */
     public static int[] recur_extend_euclid(int a,int b){
         int[] arr;
         if (b == 0){
@@ -17,12 +22,23 @@ public class Euclid{
             arr[1] = arr[2];
             arr[2] = temp;   
         }
-        System.out.print(a + "\t" + b + "\t");
-        for(int x : arr){
-            System.out.print(x + "\t");
-        }
-        System.out.println();
         return arr;
+    }
+
+    /*
+    * 求解模线性方程 ax = b (mod n)
+    */
+    public static void modular_linear_equation(int a,int b,int n){
+        int[] arr = recur_extend_euclid(a, n);
+        if (b % arr[0] == 0){
+            int x0 = (arr[1] * (b / arr[0])) % (n / arr[0]);
+            for (int i = 0;i < arr[0];i++){
+                System.out.print(((x0 + i * (n / arr[0])) % n) + "\t");
+            }
+            System.out.println();
+        }else {
+            System.out.println("no solutions");
+        }
     }
 
     public static int iter_euclid(int a,int b){
@@ -36,7 +52,10 @@ public class Euclid{
     }
 
     public static void main(String[] args){
-        System.out.println("a\tb\td\tx\ty");
-        System.out.println(iter_euclid(899, 493));
+        modular_linear_equation(3, 6, 9);
+        modular_linear_equation(1, 2, 9);
+
+        modular_linear_equation(2, 3, 7);
+        modular_linear_equation(4, 6, 7);
     }
 }
