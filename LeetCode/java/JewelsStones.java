7cdd4fb3ea3572c6a5de2dc07495012c6502659a
lsry <lsrycx@qq.com>
2019-05-12 12:54:29 +0800

    some modified

--- LeetCode/java/JewelsStones.java
+++ LeetCode/java/JewelsStones.java
@@ -1,5 +1,4 @@
 import java.util.HashMap;
-import java.util.Map;
 
 public class JewelsStones{
     public static int getJewelStones(String J,String S){
--- LeetCode/java/LeetCode0142.java
+++ LeetCode/java/LeetCode0142.java
@@ -16,6 +16,26 @@ public class LeetCode0142{
     }
 
     public ListNode detectCycle(ListNode head){
-        
+        ListNode h1 = head;
+        ListNode h2 = head;
+        boolean flag = false;
+        while (h2 != null && h2.next != null){
+            h1 = h1.next;
+            h2 = h2.next.next;
+            if (h1 == h2){
+                flag = true;
+                break;
+            }
+        }
+        if (flag){
+            h2 = head;
+            while (h1 != h2){
+                h1 = h1.next;
+                h2 = h2.next;
+            }
+            return h1;
+        } else {
+            return null;
+        }
     }
 }
